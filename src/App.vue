fbb2b12dd00c7fec64b4e1127fbaab0f38e9e6b2
dainokung <dainokung2535@gmail.com>
2019-07-18 18:04:03 +0700

    vue cropper

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="file" @change="getFile">
+    <button @click="cropImg">Crop</button>
+    <div class="preview-img" v-if="isRender">
+        <vue-cropper
+          ref="cropper"
+          :src="imgSrc"
+          alt="Source Image"
+          drag-mode="crop"
+          :aspect-ratio="1/1"
+          :zoomable="false"
+          :img-style="config"
+        >
+        </vue-cropper>
+    </div>
+    <img ref="myImg"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import VueCropper from 'vue-cropperjs';
+import 'cropperjs/dist/cropper.css';
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return {
+      imgSrc:'',
+      isRender:false,
+      config:{
+        width:'100%',
+        height:'100%',
+
+      }
+    }
+  },
+  methods:{
+    getFile(e){
+      let file = e.target.files[0]
+      this.isRender = false
+      this.getBase64(file)
+      .then((res)=>{
+        this.imgSrc = res
+        this.isRender = true
+        // this.$refs.myImg.setAttribute('src',res)
+      })
+    },
+    getBase64(file){
+      return new Promise((resolve,reject) => {
+        var render = new FileReader();
+        render.readAsDataURL(file);
+        render.onload = function() {
+          resolve(render.result)
+        }
+        render.onerror = function (error) {
+          reject(error)
+        };
+      })
+    },
+    cropImg(){
+      let el = this.$refs.cropper
+      let data = el.getData()
+      el.getCroppedCanvas(data).toBlob((blob)=>{
+        var reader = new FileReader();
+          reader.readAsDataURL(blob); 
+          reader.onloadend = ()=>{ 
+            this.$refs.myImg.setAttribute('src',reader.result)
+        }
+      })
+    }
+  },
+  components: { VueCropper }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  .preview-img{
+    width:450px;
+    height: auto;
+  }
 </style>
